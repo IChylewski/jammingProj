9000f07d1532603c00b91edf0338bc6479067427
IChylewski <ireneuszchylewski@gmail.com>
2021-04-19 15:00:13 +0100

    error correction

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -7,26 +7,29 @@ const Spotify = {
         if(accessToken){
             return accessToken;
         }
-
         const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
         
         if(accessTokenMatch && expiresInMatch){
             accessToken = accessTokenMatch[1];
             const expiresIn = Number(expiresInMatch[1]);
-
-            window.setTimeout(() => accessToken = '', expiresIn * 1000);
-            window.history.pushState('Access Token', null, '/');
+            const tokenExpiryDate = Date.now() + (expiresIn * 1000);
+            window.setTimeout(() => accessToken = '', tokenExpiryDate - Date.now());
+            //window.history.pushState('Access Token', null, '/');
             return accessToken;
         } else {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&scope=playlist-modify-public`;
             window.location = accessUrl;
         }
 
- },
+    },
+
     search(term){
+    if(!accessToken){
         accessToken = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,{
+    }
+
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${localStorage.term}`,{
             headers: {Authorization:  `Bearer ${accessToken}`}
         }).then(response => response.json())
         .then(jsonResponse =>{
@@ -45,6 +48,7 @@ const Spotify = {
             })
         })
     },
+    
     savePlaylist(name, trackURIs){
         if(!name && !trackURIs){
             return;
